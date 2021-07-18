97066acdbc23153ee735dbd0f0f68c5463a49a08
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-19 01:56:47 +0900

    new api 사용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=bae68fcaf1e24a9a980327c4e6ccd384',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
